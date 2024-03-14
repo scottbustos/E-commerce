744da8222749572217cbd2f6838c5d30a8c3f258
Scott <scobustos@gmail.com>
2024-03-14 18:55:54 -0500

    Working on the bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,79 @@
+import { Search, ShoppingCartOutlined} from '@mui/icons-material'
+import { Badge } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
 height: 60px;
-background-color: black;
+`
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+`
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+`
+const Language = styled.span`
+font-size: 14px;
+cursor: pointer;
+`
+const Searchcontainer = styled.div`
+display: flex;
+border: 0.5px solid lightgray;
+margin-left: 25px;
+padding: 5px;
+align-items: center;
+`
+const Input = styled.input`
+border: none;
+`
+
+const Center = styled.div`
+text-align: center;
+justify content: flex-end;
+flex: 1;
+`
+const Logo = styled.h1`
+
+`
+
+const Right = styled.div`
+display: flex;
+align-items: center;
+flex: 1;
+`
+const Menuitem = styled.div`
+font-size: 14px;
+cursor: pointer;
 `
 
 const Navbar = () => {
   return (
     <Container>
-        navbars
+        <Wrapper>
+            <Left>
+                <Language>EN</Language>
+                <Searchcontainer>
+                    <Input/>
+                    <Search/>
+                </Searchcontainer>
+            </Left>
+            <Center><Logo>PENGUIN.</Logo></Center>
+            <Right>
+                <Menuitem>Register</Menuitem>
+                <Menuitem>SIGN IN</Menuitem>
+                <Menuitem>
+                <Badge badgeContent={2} color="primary">
+                    <ShoppingCartOutlined/>
+                </Badge>
+                </Menuitem>
+            </Right>
+        </Wrapper>
     </Container>
   )
 }
