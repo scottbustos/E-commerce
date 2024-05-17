eab37dbea3b01c436d0174f240a8ae58239356ba
Scott <scobustos@gmail.com>
2024-05-16 22:09:02 -0500

    Added some new products

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -5,6 +5,11 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 
 const Container = styled.div``
+const Wrapper = styled.div``
+const ImgContainer = styled.div``
+const Image = styled.div``
+const InfoContainer = styled.div``
+
 
 const Product = () => {
   return (
@@ -12,7 +17,18 @@ const Product = () => {
         <Navbar/>
         <Announcement/>
         <Wrapper>
-            
+          <ImgContainer>
+            <Image src="" />
+          </ImgContainer>
+          <InfoContainer>
+              <Title>Denim Jumpsuit</Title>
+              <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+              molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+              numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+              optio, eaque rerum!</Desc>
+              <Price>$ 20</Price>
+          </InfoContainer>
+
         </Wrapper>
         <Newsletter/>
         <Footer/>
