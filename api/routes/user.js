479a23fb442bb372cb9768d51016438958e1aaa8
Scott <scobustos@gmail.com>
2024-10-28 18:04:19 -0500

    Added more CRUD functionality

--- api/routes/user.js
+++ api/routes/user.js
@@ -19,5 +19,55 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 
 
 });
+//GET ALL USERS
+router.get("/", verifyTokenAndAuthorization, async (req, res) => {
+    const query = req.query.new;
+    try {
+        const users = query ? await User.find().sort({_id: -1}).limit(5) : await User.find({});
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+//GET USER BY ID
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET USER STATS
+router.get("/stats", verifyTokenAndAuthorization, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+    try {
+        const data = await User.aggregate([
+            {$match: {createdAt: {$gte: lastYear}}},
+            {$project: {_id: 0, month: {$month: "$createdAt"}}},
+            {$group: {_id: "$month", total: {$sum: 1}}}
+        ]);
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router
--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -23,7 +23,7 @@ const verifyToken = (req, res, next) => {
 };
 const verifyTokenAndAuthorization = (req, res, next) => {
     verifyToken(req, res, () => {
-        if (req.user.id === req.params.id || req.user.isAdmin) {
+        if  (req.user.isAdmin) {
             next();
         } else {
             res.status(403).json("You are not allowed to do that!");
