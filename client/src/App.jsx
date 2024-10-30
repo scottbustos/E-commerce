fa56eeaf2cefa751a6d9290b88eea777290d7faa
Scott <scobustos@gmail.com>
2024-10-29 20:23:50 -0500

    Added react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,26 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const App = () => {
-  return <div>
-    <ProductList/>
-  </div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/"/>
+        <Route path="/products/:category" />
+        <Route path="/product/:id" />
+        <Route path="/cart" />
+        <Route path="/login" />
+        <Route path="/register" />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
